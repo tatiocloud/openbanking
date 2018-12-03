739a7ecf582f5fe20bdff4698b34d5e7858cfd0e
tatio1975 <Mechanical1975>
2018-12-03 11:24:01 +0000

    Adding Spring Boot Capability to the project with skeleton code and test to ewnsure it runs without any services.

---
+++ src/main/java/io/ram/openbanking/OpenbankingApplication.java
@@ -0,0 +1,11 @@
+package io.ram.openbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbankingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenbankingApplication.class);
+    }
+}
---
+++ src/test/java/io/ram/openbanking/OpenbankingApplicationTest.java
@@ -0,0 +1,20 @@
+package io.ram.openbanking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class OpenbankingApplicationTest {
+
+    @Test
+    public void contextLoads(){
+
+    }
+
+}
